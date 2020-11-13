799a5f5fe399ff0f51df38fc8235e7d3f811bc24
zhangjian <zhangjian@qq.com>
2020-11-13 15:48:43 +0800

    v1.2

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+module.exports = {
+    // 基本路径 baseURL已经过时
+    publicPath: process.env.NODE_ENV === 'production'?'/BBS/':'/',
+    configureWebpack: {
+        resolve: {
+          alias: {
+            'src': path.join(__dirname, '/src'), //这些路径改改看看
+            'components': path.join(__dirname, './src/components'),
+            'router': path.join(__dirname, './src/router'),
+            'assets': path.join(__dirname, './src/assets'),
+            'views': path.join(__dirname, './src/views'),
+          }
+        }
+      }
+   }
