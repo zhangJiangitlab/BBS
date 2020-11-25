b5cf9688a433893508999065641084edc5290fba
zhangjian <zhangjian@qq.com>
2020-11-25 11:39:26 +0800

    dist

--- src/router/index.js
+++ src/router/index.js
@@ -47,7 +47,7 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: "hash",
   routes
 });
 
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,15 @@
-const path = require("path");
+// const path = require("path");
 module.exports = {
-  // 基本路径 baseURL已经过时
   publicPath: process.env.NODE_ENV === "production" ? "/BBS/" : "/",
-  configureWebpack: {
-    resolve: {
-      alias: {
-        src: path.join(__dirname, "/src"), //这些路径改改看看
-        components: path.join(__dirname, "./src/components"),
-        router: path.join(__dirname, "./src/router"),
-        assets: path.join(__dirname, "./src/assets"),
-        views: path.join(__dirname, "./src/views")
-      }
-    }
-  }
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       src: path.join(__dirname, "/src"), //这些路径改改看看
+  //       components: path.join(__dirname, "./src/components"),
+  //       router: path.join(__dirname, "./src/router"),
+  //       assets: path.join(__dirname, "./src/assets"),
+  //       views: path.join(__dirname, "./src/views")
+  //     }
+  //   }
+  // }
 };
